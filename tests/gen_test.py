861bf86ad05dbeebbccac57c91512d2f3e202062
trun <e.s.trunov@gmail.com>
2024-11-18 15:51:32 +0700

    add tests

--- tests/gen_test.py
+++ tests/gen_test.py
@@ -4,18 +4,18 @@ from pathlib import Path
 
 
 @pytest.mark.parametrize(
-    "file1, file2, file_answer, form_name",
+    "filepath1, filepath2, file_answer, form_name",
     [
-        ('file1.json', 'file2.json', 'answer_stylish.txt', 'stylish'),
-        ('file1.yml', 'file2.yml', 'answer_stylish.txt', 'stylish'),
-        ('file1.json', 'file2.json', 'answer_plain.txt', 'plain'),
-        ('file1.yml', 'file2.yml', 'answer_plain.txt', 'plain'),
-        ('file1.json', 'file2.json', 'answer_json.json', 'json'),
-        ('file1.yml', 'file2.yml', 'answer_json.json', 'json'),
+        ('filepath1.json', 'filepath2.json', 'answer_stylish.txt', 'stylish'),
+        ('filepath1.yml', 'filepath2.yml', 'answer_stylish.txt', 'stylish'),
+        ('filepath1.json', 'filepath2.json', 'answer_plain.txt', 'plain'),
+        ('filepath1.yml', 'filepath2.yml', 'answer_plain.txt', 'plain'),
+        ('filepath1.json', 'filepath2.json', 'answer_json.json', 'json'),
+        ('filepath1.yml', 'filepath2.yml', 'answer_json.json', 'json'),
     ]
 )
-def test_generate_diff(file1, file2, file_answer, form_name):
+def test_generate_diff(filepath1, filepath2, file_answer, form_name):
     with open(Path() / 'tests/fixtures' / file_answer) as f:
         corr_answer = f.read()
-    assert generate_diff(file1, file2, form_name) == corr_answer
+    assert generate_diff(filepath1, filepath2, form_name) == corr_answer
     
