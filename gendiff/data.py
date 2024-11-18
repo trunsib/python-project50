2584c0c38ae392af05e38b0cef9c496343b52781
trun <e.s.trunov@gmail.com>
2024-11-18 15:45:46 +0700

    add gendiff

--- gendiff/data.py
+++ gendiff/data.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 def get_dict_from_file(path_file):
     file_ext = Path(path_file).suffix
-    path_file - Path() / 'tests/fixtures' / os.path.basename(path_file)
+    path_file = Path() / 'tests/fixtures' / os.path.basename(path_file)
     with open(path_file) as f:
         s = f.read()
     return open_file(s, file_ext)
